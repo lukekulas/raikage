7d34e246cc8ecb3e5e7447e3f6b795f34049dd03
Luke Calubaquib <kulas.calubaquib@gmail.com>
2019-07-28 19:26:41 +0000

    Set up maps integration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_27_220432) do
+ActiveRecord::Schema.define(version: 2019_07_28_185939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_07_27_220432) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.float "latitute"
+    t.float "longitude"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
