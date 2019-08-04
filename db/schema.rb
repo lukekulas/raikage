05d34d1b326dfd51643b9ae8ea5eb5a2eda7752e
Luke Calubaquib <kulas.calubaquib@gmail.com>
2019-08-04 20:29:17 +0000

    Added google maps

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2019_07_28_185939) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.float "latitute"
+    t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
